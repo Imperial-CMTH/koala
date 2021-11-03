c115f2f73b824e8d4d3bc732320172aff2e21822
Tom Hodson <thomas.c.hodson@gmail.com>
2021-11-03 10:37:21 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="topo-kwant",
+    version='0.0',
+    description='Topological quantum system simulations',
+    author="Peru D'Ornellas, Gino Cassella, Tom Hodson",
+    # author_email='',
+    packages=find_packages(),
+)
