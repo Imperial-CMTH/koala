d4ae361c5f1e81b1108da2edaedfa4552d45ddb6
Ettore <ettore@pop-os.localdomain>
2021-11-04 15:03:56 +0000

    Added edge coloring argument to plotting procedure

--- src/topamorph/plotting.py
+++ src/topamorph/plotting.py
@@ -2,17 +2,25 @@ import numpy as np
 from matplotlib.collections import LineCollection
 from matplotlib import pyplot as plt
 
-
-def plot_lattice(vertices, adjacency):
+def plot_lattice(vertices, adjacency, edge_colors = None):
 
     edge_vertices = vertices[adjacency]
     displacements = edge_vertices[:,0,:] - edge_vertices[:,1,:]
 
     mask = np.round(displacements)
     inside_bool =  np.logical_not(mask.any(axis=1))
+    inside_idx = np.where(inside_bool)
+    outside_idx = np.where(np.logical_not(inside_bool))
 
-    inside_edges = adjacency[np.where(inside_bool)]
-    outside_edges = adjacency[np.where(np.logical_not(inside_bool))]
+    inside_edges = adjacency[inside_idx]
+    outside_edges = adjacency[outside_idx]
+
+    if edge_colors is not None:
+        inside_colors = edge_colors[inside_idx]
+        outside_colors = edge_colors[outside_idx]
+    else:
+        inside_colors = 'k'
+        outside_colors = 'k'
 
     inside_edge_vertices = vertices[inside_edges]
     outside_edge_vertices = vertices[outside_edges]
@@ -26,13 +34,11 @@ def plot_lattice(vertices, adjacency):
     first_changed[:,0,:] -= outside_mask
     second_changed[:,1,:] += outside_mask
 
-    fig = plt.figure()
-    ax = plt.gca()
-    ax.set_xlim((0,1))
-    ax.set_ylim((0,1))
-    lc_inside = LineCollection(inside_edge_vertices,colors= 'k')
-    lc_first = LineCollection(first_changed,colors= 'k')
-    lc_second = LineCollection(second_changed,colors= 'k')
+    fig, ax = plt.subplots()
+    ax.set(xlim = (0,1), ylim = (0,1))
+    lc_inside = LineCollection(inside_edge_vertices, colors = inside_colors)
+    lc_first = LineCollection(first_changed, colors = outside_colors)
+    lc_second = LineCollection(second_changed, colors = outside_colors)
     ax.add_collection(lc_inside)
     ax.add_collection(lc_first)
     ax.add_collection(lc_second)
