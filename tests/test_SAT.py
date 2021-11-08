14ecc2f8c5c318fab9cbdefdd6b63192cd405d7f
Tom Hodson <thomas.c.hodson@gmail.com>
2021-11-08 18:11:42 +0000

    Add a test case to SAT tests

--- tests/test_SAT.py
+++ tests/test_SAT.py
@@ -1,3 +1,7 @@
+#TODO 
+#   - test for all_solutions argument 
+#   - test for unsolveable models and returning the minimum failing graph.
+
 def test_vertex_coloring():
     n = 40
     from koala.pointsets import generate_random
@@ -39,3 +43,51 @@ def test_edge_coloring():
     ns = np.array([edge_neighbours(i, adjacency) for i in range(adjacency.shape[0])])
     cs = colors[ns]
     assert(not np.any([colors[i] in cs[i] for i in range(colors.shape[0])]))
+
+
+"""
+unsolveable = PBC_Voronoi(vertices=array([[0.21475886, 0.25115016],
+       [0.15074368, 0.28779049],
+       [0.7637336 , 0.04206213],
+       [0.77362058, 0.08284606],
+       [0.60367343, 0.85501055],
+       [0.20977552, 0.54240834],
+       [0.44615917, 0.7539095 ],
+       [0.57293506, 0.78291032],
+       [0.67351609, 0.42529479],
+       [0.64434817, 0.64459786]]), adjacency=array([[0, 1],
+       [8, 0],
+       [8, 3],
+       [2, 3],
+       [5, 6],
+       [7, 6],
+       [7, 9],
+       [8, 9],
+       [1, 5],
+       [4, 7],
+       [4, 0],
+       [4, 2],
+       [9, 5],
+       [0, 4],
+       [1, 3],
+       [3, 1],
+       [5, 9],
+       [6, 2]]), adjacency_crossing=array([[ 0,  0],
+       [ 0,  0],
+       [ 0,  0],
+       [ 0,  0],
+       [ 0,  0],
+       [ 0,  0],
+       [ 0,  0],
+       [ 0,  0],
+       [ 0,  0],
+       [ 0,  0],
+       [ 0,  1],
+       [ 0,  1],
+       [ 1,  0],
+       [ 0, -1],
+       [-1,  0],
+       [ 1,  0],
+       [-1,  0],
+       [-1,  1]]))
+"""
