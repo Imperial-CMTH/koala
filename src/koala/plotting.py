ddb9db098958ef33522a749120ae0950aab1b4b3
Tom Hodson <thomas.c.hodson@gmail.com>
2021-11-08 18:13:09 +0000

    refactor plotting to take new args

--- src/koala/plotting.py
+++ src/koala/plotting.py
@@ -2,15 +2,22 @@ import numpy as np
 from matplotlib.collections import LineCollection
 from matplotlib import pyplot as plt
 
-def plot_lattice(vertices, adjacency, edge_colors = None, vertex_colors = None, scatter_args = None):
+def plot_lattice(vertices, adjacency, adjacency_crossing, ax = None, edge_colors = None, vertex_colors = None, scatter_args = None):
+    """"
+
+    Args:
+
+        adjacency_crossing: np.ndarray (n_edges, 2), 
+    """
+    if ax is None: ax = plt.gca()
 
     edge_vertices = vertices[adjacency]
     displacements = edge_vertices[:,0,:] - edge_vertices[:,1,:]
 
-    mask = np.round(displacements)
-    inside_bool =  np.logical_not(mask.any(axis=1))
-    inside_idx = np.where(inside_bool)
-    outside_idx = np.where(np.logical_not(inside_bool))
+    mask = np.any(adjacency_crossing != 0, axis = -1)
+    outside_idx = np.where(mask)[0]
+    inside_idx = np.where(np.logical_not(mask))[0]
+    print('mask', mask)
 
     inside_edges = adjacency[inside_idx]
     outside_edges = adjacency[outside_idx]
@@ -20,28 +27,21 @@ def plot_lattice(vertices, adjacency, edge_colors = None, vertex_colors = None, 
         outside_colors = edge_colors[outside_idx]
     else:
         inside_colors = 'k'
-        outside_colors = 'k'
+        outside_colors = 'r'
 
     inside_edge_vertices = vertices[inside_edges]
     outside_edge_vertices = vertices[outside_edges]
 
-    outside_displacements = outside_edge_vertices[:,0,:] - outside_edge_vertices[:,1,:]
-    outside_mask = np.round(outside_displacements)
-
-    first_changed = outside_edge_vertices.copy()
-    second_changed = outside_edge_vertices.copy()
-
-    first_changed[:,0,:] -= outside_mask
-    second_changed[:,1,:] += outside_mask
-
-    fig, ax = plt.subplots()
-    ax.set(xlim = (0,1), ylim = (0,1))
     lc_inside = LineCollection(inside_edge_vertices, colors = inside_colors)
-    lc_first = LineCollection(first_changed, colors = outside_colors)
-    lc_second = LineCollection(second_changed, colors = outside_colors)
     ax.add_collection(lc_inside)
-    ax.add_collection(lc_first)
-    ax.add_collection(lc_second)
+
+    for i, sign in enumerate([-1, 1]):
+        temp =  outside_edge_vertices.copy()
+        temp[:, i, :] = temp[:, i, :] + sign * adjacency_crossing[outside_idx]
+        lc_outside = LineCollection(temp, colors = outside_colors)
+        ax.add_collection(lc_outside)
+
+    ax.set(xlim = (0,1), ylim = (0,1))
     if scatter_args is not None: ax.scatter(
         vertices[:,0],
         vertices[:,1],
@@ -49,4 +49,4 @@ def plot_lattice(vertices, adjacency, edge_colors = None, vertex_colors = None, 
         **scatter_args,
     )
 
-    #TODO - look at what this should return
+    return ax
