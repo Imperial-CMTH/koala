b8b4a168208ff5e2d7faea988712278a44e763d4
Peru d'Ornellas <peru.dornellas@gmail.com>
2021-11-10 11:11:07 +0000

    fixed some mistakes

--- tests/impossible_graphs.py
+++ tests/impossible_graphs.py
@@ -3,48 +3,6 @@ from matplotlib import pyplot as plt
 from matplotlib.collections import LineCollection
 
 
-def plot_lattice(vertices, adjacency):
-
-    edge_vertices = vertices[adjacency]
-    displacements = edge_vertices[:, 0, :] - edge_vertices[:, 1, :]
-
-    mask = np.round(displacements)
-    inside_bool = np.logical_not(mask.any(axis=1))
-
-    inside_edges = adjacency[np.where(inside_bool)]
-    outside_edges = adjacency[np.where(np.logical_not(inside_bool))]
-
-    inside_edge_vertices = vertices[inside_edges]
-    outside_edge_vertices = vertices[outside_edges]
-
-    outside_displacements = outside_edge_vertices[:,
-                                                  0, :] - outside_edge_vertices[:, 1, :]
-    outside_mask = np.round(outside_displacements)
-
-    first_changed = outside_edge_vertices.copy()
-    second_changed = outside_edge_vertices.copy()
-
-    first_changed[:, 0, :] -= outside_mask
-    second_changed[:, 1, :] += outside_mask
-
-    fig = plt.figure()
-    ax = plt.gca()
-    ax.set_xlim((0, 1))
-    ax.set_ylim((0, 1))
-    lc_inside = LineCollection(inside_edge_vertices, colors='k')
-    lc_first = LineCollection(first_changed, colors='k')
-    lc_second = LineCollection(second_changed, colors='k')
-    ax.add_collection(lc_inside)
-    ax.add_collection(lc_first)
-    ax.add_collection(lc_second)
-    # ax.scatter(
-    #     vertices[:,0],
-    #     vertices[:,1]
-    # )
-
-    # TODO - look at what this should return
-
-
 def rotate(vector, angle):
     rm = np.array([
         [np.cos(angle), -np.sin(angle)],
@@ -63,8 +21,8 @@ def create_peterson_graph():
         points_outer[n, :] = rotate(points_outer[n, :], a)
         points_inner[n, :] = rotate(points_inner[n, :], a)
 
-    points = np.concatenate((points_outer, points_inner))
-    points += 0.5
+    vertices = np.concatenate((points_outer, points_inner))
+    vertices += 0.5
 
     outer_ring_adj = np.concatenate((np.arange(0, 5), np.roll(
         np.arange(0, 5), -1))).reshape((-1, 2), order='F')
@@ -76,10 +34,4 @@ def create_peterson_graph():
     adjacency = np.concatenate(
         (outer_ring_adj, between_ring_adj, inner_ring_adj))
 
-    return points, adjacency
-
-
-points, adj = create_peterson_graph()
-
-plot_lattice(points, adj)
-plt.show()
+    return vertices, adjacency
