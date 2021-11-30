0cfdcdc928a1ad7166e5df080090d827c2e50ab7
gcassella <g.cassella20@imperial.ac.uk>
2021-11-30 16:02:50 +0000

    Remove amorphouslattice from init

--- src/koala/__init__.py
+++ src/koala/__init__.py
@@ -1,4 +1,4 @@
-#https://stackoverflow.com/questions/1944569/how-do-i-write-good-correct-package-init-py-files
-__all__ = ['AmorphousLattice', ] #contrls what you get when you do from package import *
+# #https://stackoverflow.com/questions/1944569/how-do-i-write-good-correct-package-init-py-files
+# __all__ = ['AmorphousLattice', ] #contrls what you get when you do from package import *
 
-from .amorphous_lattice import AmorphousLattice
+# from .amorphous_lattice import AmorphousLattice
