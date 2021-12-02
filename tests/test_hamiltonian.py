6d05df4028d34ba40172a82bbedd10b86297b08d
gcassella <gwmcassella@gmail.com>
2021-12-02 20:57:05 +0000

    Fixed indentation error in Ham tests

--- tests/test_hamiltonian.py
+++ tests/test_hamiltonian.py
@@ -8,24 +8,24 @@ from koala.graph_color import edge_color
 from koala.hamiltonian import *
 
 def test_sublattice_labelling():
-    n = 20
-    points = generate_bluenoise(30,n,n)
-    g = generate_lattice(points)
-    solvable, solution = edge_color(g.edges.indices, n_colors = 3)
-    # bisect lattice, dimerizing along 0 bonds
-    l = bisect_lattice(g, solution, 0)
-    nverts = l.vertices.shape[0]
-    # loop over points in the lattice
-    for v_idx in range(l.vertices.shape[0]):
-      # find the edge containing v with color 0
-      e_idx = np.any(v_idx == l.edges.indices,axis=-1) & (solution == 0)
-      e_verts = l.edges.indices[e_idx][0]
-      # ensure that the two ends of this edge correspond to distinct
-      # sublattices
-      assert(
-        ((e_verts[0] >= (nverts // 2)) & (e_verts[1] < (nverts // 2)))
-          | ((e_verts[0] < (nverts // 2)) & (e_verts[1] >= (nverts // 2)))
-      )
+  n = 20
+  points = generate_bluenoise(30,n,n)
+  g = generate_lattice(points)
+  solvable, solution = edge_color(g.edges.indices, n_colors = 3)
+  # bisect lattice, dimerizing along 0 bonds
+  l = bisect_lattice(g, solution, 0)
+  nverts = l.vertices.shape[0]
+  # loop over points in the lattice
+  for v_idx in range(l.vertices.shape[0]):
+    # find the edge containing v with color 0
+    e_idx = np.any(v_idx == l.edges.indices,axis=-1) & (solution == 0)
+    e_verts = l.edges.indices[e_idx][0]
+    # ensure that the two ends of this edge correspond to distinct
+    # sublattices
+    assert(
+      ((e_verts[0] >= (nverts // 2)) & (e_verts[1] < (nverts // 2)))
+        | ((e_verts[0] < (nverts // 2)) & (e_verts[1] >= (nverts // 2)))
+    )
 
 def test_hamiltonians():
   n = 20
