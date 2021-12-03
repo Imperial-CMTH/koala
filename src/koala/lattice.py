94d60b75e65fb0cdd84ecef002f32d8b705cd42a
dpreuo <peru.dornellas@gmail.com>
2021-12-03 12:45:42 +0000

    added plaquette finding to the lattice class

--- src/koala/lattice.py
+++ src/koala/lattice.py
@@ -6,7 +6,7 @@ from dataclasses import dataclass
 
 @dataclass
 class Plaquette:
-    """Represents a single plaquette in a lattice
+    """Represents a single plaquette in a lattice. Not a list since plaquettes can have varying size.
 
     :param vertices: Indices correspondng to the vertices that border the plaquette. These are always organised to start from the lowest index and then go clockwise around the plaquette
     :type vertices: np.ndarray[int] (plaquettesize)
@@ -51,7 +51,7 @@ class Vertices:
     :param positions: List of the positions of every vertex
     :type positions: np.ndarray[float] (nvertices, 2)
     :param adjacent_edges: Lists the indices of every edge that connects to that vertex. Listed in clockwise order from the lowest index
-    :type adjacent_edges: np.ndarray[int] (nvertices, 3)
+    :type adjacent_edges: list[np.ndarray] (nvertices, n_edges_per_vertex)
     :param adjacent_plaquettes: Lists the indices of every plaquette that touches the vertex
     :type adjacent_plaquettes: np.ndarray[int] (nvertices, 3)
     """
@@ -89,15 +89,16 @@ class Lattice(object):
             adjacent_plaquettes = None # <---------------------------------------- missing
         )
 
-        # self.plaquettes  <---------------------------------------- missing
+        self.plaquettes = _find_all_plaquettes(self.edges.indices, self.vertices.adjacent_edges)
+        # <---------------------------------------- missing plaquette centers!!
 
 
 def _sorted_vertex_adjacent_edges(
     vertex_positions,
     edge_indices,
     edge_vectors):
-    """Gives you an array where the i'th row contains the indicesof the edges that connect to the i'th vertex. 
-    The edges are always organised in clockwise order, which will be handy later ;)
+    """Gives you an array where the i'th row contains the indices of the edges that connect to the i'th vertex. 
+    The edges are always organised in clockwise order starting from 12:00, which will be handy later ;)
 
     :param vertex_positions: List of the positions of every vertex
     :type vertex_positions: np.ndarray[float] (nvertices, 2)
@@ -105,15 +106,15 @@ def _sorted_vertex_adjacent_edges(
     :type edge_indices: np.ndarray[int] (nedges, 2)
     :param edge_vectors: Vectors pointing along each edge
     :type edge_vectors: np.ndarray[float] (nedges, 2)
-    :return: Array containing the indices of the three edges that connect to each point, ordered clockwise around the point.
-    :rtype: list[int] (nvertices, 3)
+    :return: List containing the indices of the edges that connect to each point, ordered clockwise around the point.
+    :rtype: list[int] (nvertices, nedges_per_vertex)
     """
 
     vertex_adjacent_edges = []
     for index in range(vertex_positions.shape[0]):
         v_edges = list(np.nonzero((edge_indices[:,0] == index )+ (edge_indices[:,1] ==  index))[0])
 
-
+        # sorts these lists to make sure that they always are ordered clockwise from 12:00 like on a clock face 
         vertex_adjacent_edges = []
         for index in range(vertex_positions.shape[0]):
             v_edges = np.nonzero((edge_indices[:,0] == index )+ (edge_indices[:,1] ==  index))[0]
@@ -140,10 +141,99 @@ def _sorted_vertex_adjacent_edges(
     return vertex_adjacent_edges
 
 
+def _find_plaquette(
+    starting_edge: int,
+    starting_direction: int,
+    edge_indices: npt.NDArray[np.integer],
+    vertex_adjacent_edges: list[npt.NDArray[np.integer]]):
+    """Given a single edge, and a direction, this code finds theplaquette corresponding to starting in that 
+    direction and only taking left turns. This means plaquettes are ordered anticlockwise - which amounts to going round each vertex clockwise.
+
+    :param starting_edge: Index of the edge where you start
+    :type starting_edge: int
+    :param starting_direction: Direction to take the first step. 0 means the same direction as the edge, 1 means opposite
+    :type starting_direction: int (0 or 1)
+    :param edge_indices: array containing the edges in the lattice
+    :type edge_indices: npt.NDArray[np.integer]
+    :param vertex_adjacent_edges: list of all edges that start at each vertex
+    :type vertex_adjacent_edges: list[npt.NDArray[np.integer]]
+    :return: A plaquette object representing the found plaquette
+    :rtype: Plaquette
+    """
+    # TODO - fix it so we also find the plaquette centers - left this out because PBC is a pain
+    # TODO - Decide when you have an open boundaries exterior plaquette!
+
+    start_vertex = edge_indices[starting_edge,starting_direction]
+    current_edge = starting_edge
+    current_vertex = start_vertex
+    current_direction = starting_direction
+    # print(current_vertex, current_edge ,edge_indices[current_edge], current_direction)
+
+    plaquette_edges = [starting_edge]
+    plaquette_vertices = [start_vertex]
+    plaquette_directions = [starting_direction]
+
+    while True:
+        # this takes one anticlockwise step around the plaquette
+        current_vertex = edge_indices[current_edge][np.where(np.roll(edge_indices[current_edge],1) == current_vertex)[0][0]]
+        current_edge_choices = vertex_adjacent_edges[current_vertex]
+        current_edge = current_edge_choices[(np.where(current_edge_choices == current_edge)[0][0] +1)% current_edge_choices.shape[0]]
+        current_direction = 0 if np.where(edge_indices[current_edge] == current_vertex)[0][0] == 0 else 1
+
+
+        # stop when you get back to where you started
+        if current_edge == starting_edge:
+            break
+        plaquette_edges.append(current_edge)
+        plaquette_vertices.append(current_vertex)
+        plaquette_directions.append(current_direction)
+
+    plaquette_edges = np.array(plaquette_edges)
+    plaquette_vertices = np.array(plaquette_vertices)
+    plaquette_directions = np.array(plaquette_directions)
+    
+    found_plaquette = Plaquette(vertices = plaquette_vertices, edges=plaquette_edges, directions = plaquette_directions, centers= None)
+
+    return found_plaquette
+
+
+def _find_all_plaquettes(
+    edge_indices: npt.NDArray[np.integer],
+    vertex_adjacent_edges: list[npt.NDArray[np.integer]]):
+    """Finds all of the plaquettes in a lattice
+
+    :param edge_indices: array containing the edges in the lattice
+    :type edge_indices: npt.NDArray[np.integer]
+    :param vertex_adjacent_edges: list of all edges that start at each vertex
+    :type vertex_adjacent_edges: list[npt.NDArray[np.integer]]
+    :return: List of all plaquettes in the lattice
+    :rtype: list[Plaquette]
+    """
+    
+    # this array checks if you've already found a plaquette on that edge going in that direction
+    edges_fwd_backwd_remaining = np.ones_like(edge_indices)
+
+    plaquettes = []
+    for i in range(edge_indices.shape[0]):
+
+    # every edge touches at most two new plaquettes one going forward and one going backwards
+        if edges_fwd_backwd_remaining[i,0] == 1:
+            plaq_obj = _find_plaquette(i,0,edge_indices,vertex_adjacent_edges)
+            edges_fwd_backwd_remaining[plaq_obj.edges,plaq_obj.directions] = 0
+            plaquettes.append(plaq_obj)
+
+        if edges_fwd_backwd_remaining[i,1] == 1:
+            plaq_obj = _find_plaquette(i,1,edge_indices,vertex_adjacent_edges)
+            edges_fwd_backwd_remaining[plaq_obj.edges,plaq_obj.directions]= 0
+            plaquettes.append(plaq_obj)
+
+
+    return plaquettes
 
 
 
 
+    # here lies old code. long live old code
 '''
 class Lattice(object):
     """Describes a lattice in 2D. Lattice is made up of vertices, edges and plaquettes.
