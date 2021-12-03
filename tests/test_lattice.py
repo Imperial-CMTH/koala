01acc2eb8f14cb21509873f2586c79bad76364bf
dpreuo <peru.dornellas@gmail.com>
2021-12-03 16:40:51 +0000

    added a fairly basic lattice test

--- tests/test_lattice.py
+++ tests/test_lattice.py
@@ -1,5 +1,9 @@
 import numpy as np
+from koala import lattice
 from koala.lattice import Lattice
+from koala.voronization import generate_lattice
+from koala.pointsets import generate_random
+from koala.example_graphs import *
 
 def test_undirected():
     """
@@ -25,3 +29,11 @@ def test_undirected():
     assert(np.all(directed.edges.indices == edge_indices))
     assert(np.all(directed.edges.crossing == edge_crossing))
     #plot_lattice(lattice, edge_arrows = True)
+
+def test_lattice_class():
+    lattice1 = tutte_graph()
+    lattice2 = tri_square_pent()
+    points = generate_random(30)
+    lattice3 = generate_lattice(points)
+
+    assert (len(lattice3.plaquettes) == 30)
