25aa3b816eaf8eacc6757ccb96a66076327e635e
Tom Hodson <thomas.c.hodson@gmail.com>
2022-01-08 15:47:04 +0100

    Add code to find zero flux sectors

---
+++ src/koala/flux_finder.py
@@ -0,0 +1,143 @@
+import numpy as np
+
+from .lattice import INVALID, Lattice
+from .pathfinding import straight_line_length, periodic_straight_line_length, path_between_plaquettes
+
+def fluxes_from_bonds(l, bonds):
+    """
+    Given a lattice l and a set of Uij +/-1 defined on the edges of the lattice, calculate the fluxes.
+    The fluxes are defined on each plaquette as the the product of each edge Uij in the direction,
+    with the sign flipeed otherwise.
+    """
+    fluxes = np.zeros(len(l.plaquettes), dtype = complex)
+    for i, p in enumerate(l.plaquettes):
+        bond_signs = bonds[p.edges] #the signs of the bonds around this plaquette
+        bond_directions = 1 - 2*p.directions #the direction of each edge expressed as +/- 1
+        fluxes[i] = - np.product(1.0j * bond_signs * bond_directions)
+    return fluxes
+
+def _random_bonds(l : Lattice) -> np.ndarray:
+    return 1 - 2*np.random.choice(2, size = l.n_edges)
+    
+def _greedy_plaquette_pairing(plaquettes, distance_func):
+    if plaquettes.shape[0] % 2 == 1: 
+        plaquettes = plaquettes[:-1]
+    
+    to_pair = set(plaquettes)
+    pairs = []
+    while to_pair:
+        cur = to_pair.pop()
+        _, closest = min(((distance_func(cur, other), other) for other in to_pair))
+        pairs.append((cur, closest))
+        to_pair.remove(closest)
+        
+    return np.array(pairs)
+
+def _remapper(input_array, mapper, output_type):
+    "A fast way to remap the values of a numpy array using a dict"
+    output_array = np.zeros(shape = input_array.shape, dtype = output_type)
+    for in_val, out_val in mapper.items(): output_array[input_array==in_val] = out_val
+    return output_array
+
+def fluxes_to_labels(fluxes):
+    m = {1. : 0, 1.0j : 1,  -1.0 : 2, -1.0j : 3}
+    return _remapper(input_array = fluxes, mapper = m, output_type = int)
+    
+def _ignore_even_odd_distinction(flux_sector) -> np.ndarray:
+    "Ignore the even/odd plaquette distinction by remapping +1/+i to +1 and -1/-i to -1"
+    m = {1. : 1, 1.0j : 1,  -1.0 : -1, -1.0j : -1}
+    return _remapper(input_array = flux_sector, mapper = m, output_type = int)
+    
+def _flip_adjacent_fluxes(l : Lattice, bonds : np.ndarray, fluxes : np.ndarray):
+    """
+    See docs for find_flux_sector, this helper method implements step 2) 
+    which is that it looks for adjacent -1 fluxes and flips their edges
+    """
+    for edge_index, (p_a, p_b) in enumerate(l.edges.adjacent_plaquettes):
+        if (p_a == INVALID) or (p_b == INVALID): break
+        if (fluxes[p_a] == -1) and (fluxes[p_b] == -1):
+            bonds[edge_index] *= -1
+            fluxes[p_a] *= -1
+            fluxes[p_b] *= -1
+    
+    #attempt at vectorising, check this at somepoint
+    #adj_fluxes = fluxes[l.edges.adjacent_plaquettes]
+    #to_flip = np.where((adj_fluxes[:, 0] == -1) & (adj_fluxes[:, 1] == -1))
+    #bonds[to_flip] *= -1
+    #fluxes_to_flip = l.edges.adjacent_plaquettes[to_flip].flatten()
+    #fluxes[fluxes_to_flip] *= -1
+    
+    return bonds, fluxes
+
+def _flip_isolated_fluxes(l : Lattice, bonds : np.ndarray, fluxes : np.ndarray):
+    """
+    See docs for find_flux_sector, this helper method implements step 3-5)
+    """
+    indices_to_flip = np.where(fluxes == -1)[0]
+    
+    def pos(p): return l.plaquettes[p].center
+    def distance_func(a,b): return straight_line_length(pos(a), pos(b))
+    close_pairs = _greedy_plaquette_pairing(indices_to_flip, distance_func)
+    
+    for a,b in close_pairs:
+        plaquettes, edges_to_flip = path_between_plaquettes(l, a, b)
+        bonds[edges_to_flip] *= -1
+        fluxes[a] *= -1
+        fluxes[b] *= -1
+    
+    return bonds, fluxes
+    
+def find_flux_sector(l: Lattice, target_flux_sector : np.ndarray = None, 
+                     initial_bond_guess : np.ndarray = None,
+                     strict = False,
+                    ):
+    """
+    Find a bond configuration that produces the given flux sector for the lattice l.
+    
+    The high level method is:
+        1) Figure out which fluxes need to flip using fluxes_to_flip = target_flux_sector / initial_flux_sector
+        2) Look for any adjacent -1 fluxes in fluxes_to_flip that can be anhilated by just flipping 1 edge.
+        3) Pair the remaining fluxes off into close-ish pairs
+        4) Use A* to find a path between each pair.
+        5) Flip all the edges along each path.
+    
+    Where:
+    A bond configuration is an assignment of +/-1 to each bond.
+    A flux sector is an asignment of +/-1 to even plaquette and +/-i to odd plaquettes.
+    When strict = False the code treats +1 and +i as the same and vice versa.
+    """
+    
+    if target_flux_sector is None: target_flux_sector = np.ones(l.n_plaquettes, dtype = int)
+    if not strict: target_flux_sector = _ignore_even_odd_distinction(target_flux_sector)
+    if initial_bond_guess is None: initial_bond_guess = _random_bonds(l)
+    
+    initial_flux_sector = fluxes_from_bonds(l, initial_bond_guess)
+    if not strict: initial_flux_sector = _ignore_even_odd_distinction(initial_flux_sector)
+    
+    # figure out which fluxes need to be flipped
+    fluxes_to_flip = target_flux_sector / initial_flux_sector
+    
+    if strict and np.any(np.imag(fluxes_to_flip)): 
+        raise ValueError("Fluxes don't seem to match the even/odd plaquettes.")
+    fluxes_to_flip = np.real(fluxes_to_flip).astype(int)
+    
+    # n_differences = np.count_nonzero(fluxes_to_flip == -1) 
+    # if n_differences % 2 == 1: 
+        # raise ValueError(f"There is an odd number ({n_differences}) of differences between the initial and target flux configurations.")
+    
+    # step 2) flip edges that join adjacent -1 fluxes
+    bonds, fluxes_to_flip = _flip_adjacent_fluxes(l, initial_bond_guess, fluxes_to_flip)
+    
+    # steps 3) 4) and 5) Use A* to flip the remainging isolated fluxes
+    bonds, fluxes_to_flip = _flip_isolated_fluxes(l, bonds, fluxes_to_flip)
+    
+    if np.count_nonzero(fluxes_to_flip == -1) > 1: 
+        raise ValueError("find_flux_sector failed to get rid of all the -1 fluxes, this is a bug.")
+    
+    found_fluxes = fluxes_from_bonds(l, bonds) 
+    if not strict: found_fluxes = _ignore_even_odd_distinction(found_fluxes)
+    
+    if np.count_nonzero(found_fluxes - target_flux_sector) > 1:
+        raise ValueError("find_flux_sector thought that it worked but somehow still didn't, a bug.")
+        
+    return bonds
---
+++ tests/test_flux_finder.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+from koala.flux_finder import find_flux_sector, fluxes_from_bonds
+from koala import voronization, pointsets, example_graphs
+from koala import graph_color
+
+def find_random_flux_sector(l):
+    """Tries to find a random flux sector."""
+    random_fluxes = np.random.choice([-1, 1], size = l.n_plaquettes)
+    bonds = find_flux_sector(l, random_fluxes)
+    fluxes = fluxes_from_bonds(l, bonds)
+    return bonds, fluxes
+
+def test_flux_finder():
+    """find_flux_sector checks for itself if it finds an output within one flux of the input"""
+    n = 15
+    honeycomb, _ = example_graphs.generate_honeycomb(n, return_colouring=True)
+    honeycomb_bonds, _ = find_random_flux_sector(honeycomb)
+    
+    random_points = pointsets.generate_random(n*n)
+    amorphous = voronization.generate_lattice(random_points, shift_vertices=True)
+    amorphous_bonds, _ = find_random_flux_sector(amorphous)
