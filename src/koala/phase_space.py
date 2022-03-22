f7fb34f3e6afe9f745aaff1ff3c3dbe07f6642c9
dpreuo <49160657+dpreuo@users.noreply.github.com>
2022-03-22 11:30:48 +0100

    normalised for factor of 1/4

--- src/koala/phase_space.py
+++ src/koala/phase_space.py
@@ -28,7 +28,8 @@ def k_hamiltonian_generator(lattice:Lattice, coloring:np.ndarray, J: np.ndarray,
         H = np.zeros((lattice.n_vertices,lattice.n_vertices), dtype = 'complex')
         for n, edge_indices in enumerate(lattice.edges.indices):
             cross = lattice.edges.crossing[n]
-            H[edge_indices[1],edge_indices[0]] = 2  * j_vals[n] * ujk[n] * np.exp(1j*np.sum(cross*k))
-        return (H - H.T)*1j
+            H[edge_indices[1],edge_indices[0]] = 0.5j  * j_vals[n] * ujk[n] * np.exp(1j*np.sum(cross*k))
+            out = H + H.conj().T
+        return out
 
     return k_hamiltonian
