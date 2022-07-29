66234de803d7fc36682e198537f4e91e1759e6e4
dpreuo <49160657+dpreuo@users.noreply.github.com>
2022-07-29 18:11:34 +0100

    added flux code that uses a modified definition of real flux - the product of ujk around each plaquete

--- src/koala/flux_finder/flux_finder.py
+++ src/koala/flux_finder/flux_finder.py
@@ -1,37 +1,111 @@
 import numpy as np
-
 from ..lattice import INVALID, Lattice
 from .pathfinding import straight_line_length, periodic_straight_line_length, path_between_plaquettes
+import functools
+import warnings
 
-def fluxes_from_bonds(lattice: Lattice, ujk: np.ndarray, real = True) -> np.ndarray:
+def deprecated(func):
+    """This is a decorator which can be used to mark functions
+    as deprecated. It will result in a warning being emitted
+    when the function is used."""
+
+    @functools.wraps(func)
+    def new_func(*args, **kwargs):
+        warnings.warn(
+            "Call to deprecated function {}.".format(func.__name__),
+            category=DeprecationWarning,
+            stacklevel=2,
+        )
+        return func(*args, **kwargs)
+
+    return new_func
+
+def fluxes_from_ujk(lattice: Lattice, ujk: np.ndarray, real = True) -> np.ndarray:
     """
     Given a lattice l and a set of bonds = +/-1 defined on the edges of the lattice, calculate the fluxes.
     The fluxes are defined on each plaquette as the the product of each bond in the direction,
     with the sign fliped otherwise.
 
-    :param lattice: The lattice.
-    :type lattice: Lattice
-    :param ujk: The bond variables +1 or -1
-    :type ujk: np.ndarray
-    :return: fluxes
-    :rtype: np.ndarray
+    Args:
+        lattice (Lattice): The lattice
+        ujk (np.ndarray): The bond variables +1 or -1
+        real (bool, optional): If true, gives the real flux according to f = f_real*(i^n) . Defaults to True.
+
+    Returns:
+        np.ndarray: The fluxes
     """
     fluxes = np.zeros(lattice.n_plaquettes, dtype = 'int') if real else np.zeros(lattice.n_plaquettes, dtype = 'complex')
 
-
     for i, p in enumerate(lattice.plaquettes):
-        bond_signs = ujk[p.edges] #the signs of the bonds around this plaquette
-
-        sign_real = np.array([1,-1,-1,1])
-        sign = sign_real[p.n_sides%4]
-
-        if not real and p.n_sides%2 == 1: 
-            sign = sign*1j
-
-        fluxes[i] = sign*np.product(bond_signs * p.directions)
-
-
+        bond_signs = ujk[p.edges]
+        if not real:
+            bond_signs = bond_signs*(1j)
+        fluxes[i] = np.prod(-bond_signs * p.directions)
     return fluxes
+
+def ujk_from_fluxes(lattice: Lattice, target_flux_sector : np.ndarray = None, 
+                     initial_ujk_guess : np.ndarray = None,
+                    ):
+    """given a target (real) flux sector, find a set of u_jk bond operators that correspond to that flux sector
+
+    Args:
+        lattice (Lattice): the lattice
+        target_flux_sector (np.ndarray, optional): the target value for flux through every plaquette. Must be real and correspond to the product of ujks around the plaquette. Defaults to None.
+        initial_bond_guess (np.ndarray, optional): a guess for the starting values of the bonds, if none then we assume all ujk = 1. Defaults to None.
+
+    Raises:
+        ValueError: find_flux_sector failed to get rid of all the -1 fluxes, this is a bug.
+        ValueError: find_flux_sector thought that it worked but somehow still didn't, a bug.
+
+    Returns:
+        _type_: _description_
+    """
+    
+    if target_flux_sector is None: target_flux_sector = np.ones(lattice.n_plaquettes, dtype = np.int8)
+    if initial_ujk_guess is None: initial_ujk_guess = np.ones(lattice.n_edges, dtype = np.int8)
+    initial_ujk_guess, target_flux_sector = initial_ujk_guess.copy(), target_flux_sector.copy()
+
+    initial_flux_sector = fluxes_from_ujk(lattice, initial_ujk_guess)
+    
+    # figure out which fluxes need to be flipped
+    fluxes_to_flip = target_flux_sector // initial_flux_sector
+
+    # step 2) flip edges that join adjacent -1 fluxes
+    bonds, fluxes_to_flip = _flip_adjacent_fluxes(lattice, initial_ujk_guess, fluxes_to_flip)
+    
+    # steps 3) 4) and 5) Use A* to flip the remainging isolated fluxes
+    bonds, fluxes_to_flip = _flip_isolated_fluxes(lattice, bonds, fluxes_to_flip)
+    
+    if np.count_nonzero(fluxes_to_flip == -1) > 1: 
+        raise ValueError("find_flux_sector failed to get rid of all the -1 fluxes, this is a bug.")
+    
+    found_fluxes = fluxes_from_ujk(lattice, bonds) 
+    
+    if np.count_nonzero(found_fluxes - target_flux_sector) > 1:
+        raise ValueError("find_flux_sector thought that it worked but somehow still didn't, a bug.")
+        
+    return bonds.astype(np.int8)
+
+def n_to_ujk_flipped(n: int, ujk: np.ndarray, min_spanning_set: np.ndarray):
+    """given an integer n in the 0 - 2^(number of edges in spanning tree), this code flips the edges of ujk in the spanning tree in the combination given by the binary representation of n. Useful for exhaustively searching over the entire flux space of a lattice.
+
+    :param n: number determining the flip configuration
+    :type n: int
+    :param ujk: the edge signs of the whole lattice
+    :type ujk: np.ndarray( ±1 )
+    :param min_spanning_set: an array containing a minimum set of edges that form a plaquete-spanning tree on the system 
+    :type min_spanning_set: np.ndarray( int )
+    :return: a flipped set of ujk
+    :rtype: np.ndarray( ±1 )
+    """
+    n_in_tree =  len(min_spanning_set)
+    str_format = '0' + str(n_in_tree) + 'b'
+    flips = np.array([int(x) for x in format(n, str_format)])
+    ujk_flipped = ujk.copy()
+    new_f_values = 1-2*flips
+    ujk_flipped[min_spanning_set] = new_f_values
+    return ujk_flipped.astype(np.int8)
+
 
 def fluxes_to_labels(fluxes: np.ndarray) -> np.ndarray:
     """Remaps fluxes from the set {1,-1} to labels in the form {0,1} for plotting.
@@ -98,7 +172,48 @@ def _flip_isolated_fluxes(l : Lattice, bonds : np.ndarray, fluxes : np.ndarray):
         fluxes[b] *= -1
     
     return bonds, fluxes
-    
+
+######################################################################################################
+######################################## deprecated functions ########################################
+######################################################################################################
+
+# these functions use an old definition of the real flux - found by taking the real or imaginary part of the imag flux
+# really it makes more sense to just use the clockwise product of ujk, thich is what the new functions use
+
+@deprecated
+def fluxes_from_bonds(lattice: Lattice, ujk: np.ndarray, real = True) -> np.ndarray:
+    """
+    Given a lattice l and a set of bonds = +/-1 defined on the edges of the lattice, calculate the fluxes.
+    The fluxes are defined on each plaquette as the the product of each bond in the direction,
+    with the sign fliped otherwise.
+
+    Args:
+        lattice (Lattice): The lattice
+        ujk (np.ndarray): The bond variables +1 or -1
+        real (bool, optional): If true, gives the real coefficient of the real or imaginary fluxes . Defaults to True.
+
+    Returns:
+        np.ndarray: The fluxes
+    """
+    fluxes = np.zeros(lattice.n_plaquettes, dtype = 'int') if real else np.zeros(lattice.n_plaquettes, dtype = 'complex')
+
+    for i, p in enumerate(lattice.plaquettes):
+        bond_signs = ujk[p.edges] #the signs of the bonds around this plaquette
+
+        sign_real = np.array([1,-1,-1,1])
+        sign = sign_real[p.n_sides%4]
+
+        if not real and p.n_sides%2 == 1: 
+            sign = sign*1j
+
+        fluxes[i] = sign*np.product(bond_signs * p.directions)
+
+
+    return fluxes
+
+
+
+@deprecated
 def find_flux_sector(lattice: Lattice, target_flux_sector : np.ndarray = None, 
                      initial_bond_guess : np.ndarray = None,
                     ):
@@ -142,22 +257,3 @@ def find_flux_sector(lattice: Lattice, target_flux_sector : np.ndarray = None,
         
     return bonds.astype(np.int8)
 
-def n_to_ujk_flipped(n: int, ujk: np.ndarray, min_spanning_set: np.ndarray):
-    """given an integer n in the 0 - 2^(number of edges in spanning tree), this code flips the edges of ujk in the spanning tree in the combination given by the binary representation of n. Useful for exhaustively searching over the entire flux space of a lattice.
-
-    :param n: number determining the flip configuration
-    :type n: int
-    :param ujk: the edge signs of the wholelattice
-    :type ujk: np.ndarray( ±1 )
-    :param min_spanning_set: an array containing a minimum set of edges that form a plaquete-spanning tree on the system 
-    :type min_spanning_set: np.ndarray( int )
-    :return: a flipped set of ujk
-    :rtype: np.ndarray( ±1 )
-    """
-    n_in_tree =  len(min_spanning_set)
-    str_format = '0' + str(n_in_tree) + 'b'
-    flips = np.array([int(x) for x in format(n, str_format)])
-    ujk_flipped = ujk.copy()
-    new_f_values = 1-2*flips
-    ujk_flipped[min_spanning_set] = new_f_values
-    return ujk_flipped.astype(np.int8)
--- tests/test_flux_finder.py
+++ tests/test_flux_finder.py
@@ -1,10 +1,11 @@
 from re import A
 import numpy as np
 
-from koala.flux_finder import find_flux_sector, fluxes_from_bonds
+from koala.flux_finder import find_flux_sector, fluxes_from_bonds, ujk_from_fluxes, fluxes_from_ujk
 from koala import voronization, pointsets, example_graphs
 from koala import graph_color
 from koala.flux_finder.pathfinding import path_between_plaquettes, path_between_vertices
+from koala.lattice import Lattice
 
 # make some lattices to test
 n = 15
@@ -12,19 +13,18 @@ honeycomb, _ = example_graphs.honeycomb_lattice(n, return_coloring=True)
 random_points = pointsets.generate_random(n*n)
 amorphous = voronization.generate_lattice(random_points, shift_vertices=True)
 
-def find_random_flux_sector(l):
-    """Tries to find a random flux sector."""
-    random_fluxes = np.random.choice([-1, 1], size = l.n_plaquettes)
-    bonds = find_flux_sector(l, random_fluxes)
-    fluxes = fluxes_from_bonds(l, bonds)
-    return bonds, fluxes
+def find_gnd_state(lattice:Lattice):
+    gnd_flux = np.array([-1]*lattice.n_plaquettes)
+    ujk = ujk_from_fluxes(lattice, gnd_flux)
+    return ujk, gnd_flux
 
-def test_flux_finder():
+def test_ujk_from_fluxes():
     """find_flux_sector checks for itself if it finds an output within one flux of the input"""
     for lattice in [honeycomb, amorphous]:
-        honeycomb_bonds, _ = find_random_flux_sector(honeycomb)
-        amorphous_bonds, _ = find_random_flux_sector(amorphous)
+        ujk, flux = find_gnd_state(lattice)
+        flux_tester = fluxes_from_ujk(lattice, ujk)
 
+        assert(np.all(flux == flux_tester))
 
 def test_pathfinding():
     for lattice in [honeycomb, amorphous]:
@@ -35,3 +35,16 @@ def test_pathfinding():
         a,b = np.random.randint(lattice.n_plaquettes, size = (2,))
         plaquettes, edges = path_between_plaquettes(lattice, a, b, early_stopping = False)
         plaquettes, edges = path_between_plaquettes(lattice, a, b, early_stopping = True)
+
+# test old flux code
+def find_random_flux_sector(l):
+    """Tries to find a random flux sector."""
+    random_fluxes = np.random.choice([-1, 1], size = l.n_plaquettes)
+    bonds = find_flux_sector(l, random_fluxes)
+    fluxes = fluxes_from_bonds(l, bonds)
+    return bonds, fluxes
+
+def test_flux_finder():
+    """find_flux_sector checks for itself if it finds an output within one flux of the input"""
+    honeycomb_bonds, _ = find_random_flux_sector(honeycomb)
+    amorphous_bonds, _ = find_random_flux_sector(amorphous)
